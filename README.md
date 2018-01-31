cc3bcd7bb9714605d3879760110e95cf90bb72f7
Anders Rune Jensen <arj03@protonmail.ch>
2018-01-31 22:15:36 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Benchmark CI visualizer
+
+Visualize the results of [benchmark ci
+runner](%ZjxP02wfmuYPQ5+ye+M3yoszJ14rJbZDwATdJ9vTWqk=.sha256)
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function serve(req, res) {
 
   for (var i = 1; i <= 11; ++i) {
     let num = String(i).padStart(2, '0')
-    let data = fs.readFileSync('../bench-ssb-share/@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519/bench-' + num + '.json')
+    let data = fs.readFileSync('../bench-ssb-share/@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519/2018-01-31_22:00:00/bench-' + num + '.json')
     let benchName = Object.keys(JSON.parse(data))[0]
 
     selectOptions.push(h('option', { value: i - 1 }, num + ' ' + benchName))
